d190046aa5c8411653ca1008ee492d572547e178
Adrian <adrian.burgos.colas@gmail.com>
2025-07-11 21:47:33 -0400

    add car-card to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, inject, signal } from "@angular/core";
 import { CarCardComponent } from "../car-card/car-card.component";
+import { CardService } from "@app/card.service";
+import { CARS_API_URL } from "@app/constants";
 
 @Component({
   selector: "app-home",
@@ -7,4 +9,10 @@ import { CarCardComponent } from "../car-card/car-card.component";
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.css",
 })
-export class HomeComponent {}
+export class HomeComponent {
+  carsService = inject(CardService);
+  carsList = signal<Car[]>([]);
+  ngOnInit() {
+    this.carsService.getCarsList(CARS_API_URL, this.carsList);
+  }
+}
